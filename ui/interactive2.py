77100978d30786c5c9e173bb18ce02040280ca71
larskouwenhoven <larskouwenhoven@me.com>
2022-05-06 13:23:08 -0400

    Update st

--- ui/interactive2.py
+++ ui/interactive2.py
@@ -7,10 +7,104 @@ import pandas as pd
 import pathlib
 import os
 from PIL import Image
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="studio")
+import osmnx as ox
+ox.config(use_cache=True, log_console=True)
+from shapely.geometry import Point
+import streamlit.components.v1 as components
+import folium
 
 dir_name = os.path.abspath(os.path.dirname(__file__))
 
+nta = gpd.read_file("../NTA_ACS_2014_2018.gdb")
+nta = nta.to_crs("EPSG:4326")
+print(nta.crs)
+hec = gpd.read_file("../Hurricane Evacuation Centers")
+hec = hec.to_crs(nta.crs)
+print(hec.crs)
+
+if 'G' not in st.session_state:
+    st.session_state.G = ox.load_graphml('../graph.graphml')
+
+G = st.session_state.G
+
 st.title('Lighthouse Technical Demo')
+
+st.subheader("Personalised routing")    
+
+with st.form(key='myform'):
+    address = st.text_input("Enter your address", "1 Times Square NYC", key="address")
+    vehicle = st.selectbox(
+        "If you have a personal vehicle, how many spare seats do you have?", 
+        ('None, I do not own a vehicle', 'None, there are no spare seats', 'One', 'Two', 'Three', 'Four', 'Five', 'Six'), 
+        key='vehicle'
+    )
+    pickup = st.checkbox(
+        "I would be open to helping someone evacuate", False, key='pickup'
+    )
+    submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        location = geolocator.geocode(st.session_state.address)
+        # st.write((location.latitude, location.longitude))
+
+        nearest_node = ox.get_nearest_node(G, (location.latitude, location.longitude))
+        closest_hec = hec.distance(Point(location.longitude, location.latitude)).argmin()
+        closest_hec_x = hec.loc[closest_hec].geometry.coords.xy[0][0]
+        closest_hec_y = hec.loc[closest_hec].geometry.coords.xy[1][0]
+
+        nearest_hec_node = ox.get_nearest_node(G, (closest_hec_y, closest_hec_x))
+
+        shortest_route = ox.distance.shortest_path(G, nearest_node, nearest_hec_node, weight='travel_time')
+
+        if not pickup:
+            route_map = ox.plot_route_folium(G, shortest_route, color='green')
+            folium.Marker(
+                [location.latitude, location.longitude], popup="<i>Origin</i>",
+            ).add_to(route_map)
+            folium.Marker(
+                [closest_hec_y, closest_hec_x], popup="<i>Destination</i>",
+            ).add_to(route_map)
+
+            route_map.save("route.html")
+
+            HtmlFile = open("route.html", 'r', encoding='utf-8')
+            source_code = HtmlFile.read() 
+            components.html(source_code, height=500)
+        elif vehicle in ["One", "Two", "Three", "Four", "Five", "Six"]:
+            origin = nearest_node
+            stop = ox.get_nearest_node(G, (location.latitude + 0.005, location.longitude + 0.015))
+            destination = nearest_hec_node
+            first_route = ox.distance.shortest_path(G, nearest_node, stop, weight='travel_time')
+            second_route = ox.distance.shortest_path(G, stop, destination, weight='travel_time')
+            full_route = first_route[:-1] + second_route
+
+            orig_route_time = int(sum(ox.utils_graph.get_route_edge_attributes(G, shortest_route, "travel_time")))
+            new_route_time = int(sum(ox.utils_graph.get_route_edge_attributes(G, full_route, "travel_time")))
+
+            m3 = ox.plot_route_folium(G, shortest_route, opacity=0.5, color='green', dash_array=10)
+            m1 = ox.plot_route_folium(G, first_route, opacity=0.5, route_map=m3)
+            m2 = ox.plot_route_folium(G, second_route, route_map=m1, opacity=0.5, color='red')
+            m0 = ox.plot_route_folium(G, full_route, opacity=0.1, route_map=m2)
+            folium.Marker(
+                [location.latitude + 0.005, location.longitude + 0.015], popup="<i>Pickup</i>", color='green'
+            ).add_to(m0)
+            folium.Marker(
+                [location.latitude, location.longitude], popup="<i>Origin</i>",
+            ).add_to(m0)
+            folium.Marker(
+                [closest_hec_y, closest_hec_x], popup="<i>Destination</i>",
+            ).add_to(m0)
+            m0.save("route.html")
+
+            HtmlFile = open("route.html", 'r', encoding='utf-8')
+            source_code = HtmlFile.read() 
+            components.html(source_code, height=500)
+
+            st.write("Originally, your evacuation travel time would have been **" + str(round(orig_route_time / 60, 2)) + " minutes**. Your travel time including pick-up is **" + str(round(new_route_time / 60, 2)) + " minutes**. By spending " + str(round((new_route_time - orig_route_time) / 60, 2)) + " additional minutes, you are helping " + vehicle.lower() + " people evacuate. Good job!")
+
+
 
 intro_text = "Lighthouse is an app that helps people get to safety in times of hurricanes and other natural disasters. Lighthouse heavily relies on algorithms that optimize the user journey when evacuating in these events. This page allows the user to understand these algorithms and the techniques behind Lighthouse through an interactive demonstration, focusing on New York City. "
 intro_text_2 = "The NYC government has devised 6 different evacuation zones. During a natural disaster,  individuals living in a subset of these evacuation zones may be required to evacuate, or conversely, to stay in place. In this technical demonstration, we will use three different scenarios. In scenario 1, evacuation zones 1 and 2 are asked to evacuate. In scenario 2, this is the case for zones 1 through 4; and finally, in scenario 3, all zones (1 through 6) are asked to evacuate. These three scenarios are displayed in the maps below. "
@@ -128,3 +222,4 @@ elif scenario_3 == 'Scenario 2':
     st.image(rs_img_2)
 elif scenario_3 == 'Scenario 3':
     st.image(rs_img_3)
+
